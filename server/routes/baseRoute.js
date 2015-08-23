d862222d57c42d74faa48b6851d83c95d935f20d
Nivesh Singh <nivesh2@gmail.com>
2015-08-24 02:02:08 +0530

    Updated to added latest routes 1.0

--- server/routes/baseRoute.js
+++ server/routes/baseRoute.js
@@ -2,9 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 var user = require('../functions/logic/user');
+var expense = require('../functions/logic/expense');
 
-router.use('/user/login',user.login);
-router.use('/user/insert',user.insertExpense);
-router.use('/user/myExpenseList/:username',user.myExpenseList);
+router.use('/user/signup/:username', user.signUp);
+router.use('/user/login/:username', user.login);
+router.use('/user/insert', user.insertExpense);
 
-module.exports = router; 
+router.use('/expense/byUser/:username', expense.myExpenseList);
+router.use('/expense/totalExpense/:expenseId', expense.totalExpense);
+router.use('/expense/addnewexpense/:username',expense.addNewExpense);
+router.use('/expense/adduser',expense.addNewUserToExpense);
+
+module.exports = router;
